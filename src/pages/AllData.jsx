9d23f39464051d2a93e7bdc77c04b9b5491cc79b
cmedinam <cmedinam@co.ibm.com>
2023-09-28 12:21:48 -0500

    Cambio all data

--- src/pages/AllData.jsx
+++ src/pages/AllData.jsx
@@ -1,12 +1,8 @@
-import { useContext } from 'react';
-import { UserContext } from '../main'
-
-
 export const AllData = () => {
 
-  const user = useContext(UserContext);
+  
 
   return (
-    <div>AllData {user.name}</div>
+    <div>All Data</div>
   )
 }
