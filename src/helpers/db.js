bee0eb23ae56869f74c7bd67f09895e0a5b2f2ac
Carlos Medina <carlosmedina@192.168.0.3>
2023-12-17 18:51:05 -0500

    Conexión api y almacenar token

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 import { getDatabase, ref, set } from "firebase/database";
 
 
-//Crear historial del usuario  
+//Crear registro usuario bd 
 export function writeUserData(uid, balance) {
     const db = getDatabase();
     set(ref(db, 'users/' + uid), {
