0244350d680c81e368e4cb730040903039055e4a
Carlos Medina <carlosmedina@MacBook-Pro-de-Carlos.local>
2024-01-02 20:06:46 -0500

    Corrección url api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-export const urlBase = 'https://https://api-badbank.azurewebsites.net/';
+export const urlBase = 'https://api-badbank.azurewebsites.net';
 //export const urlBase = 'http://localhost:3000';
